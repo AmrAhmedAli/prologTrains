39b0b0bf15d359e81624b143dd1401c53e396414
AmrAhmedAli <amr.ali-hassan@student.guc.edu.eg>
2018-12-09 04:39:04 +0200

    Update trains.pl

--- trains.pl
+++ trains.pl
@@ -246,12 +246,12 @@ trainSchedules(ST, ET, SS, ES, Conn, Route,_):-
     labeling([min(SUM)],[AET, BET, CET, DET, EET, FET, GET, HET,IET, JET, KET]),
     reply_html_page(title('Loaded Prolog modules'),
                         [ h1('Loaded Prolog modules'),
-                          table(class(table),
+                          table(class(properties),
                                 [ \header
                                 | \modules([1,2,3,4,5,6,7,8,9,10,11],ST,SS,ET,ES,Route)
                                 ]),
                             h1('Connections Table'),
-                          table(class(table),
+                          table(class(properties),
                                 [ \header2
                                 | \modules3(["FE","ED","DC","CB","BC","BA","CK","KL",
                                 "LM","LJ","JL","JH","HI","HG","GD","DG"],Conn)
